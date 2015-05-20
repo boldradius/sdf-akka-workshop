9894a010fb5eefcf40e6e8247db94dc8a195aa0c
David Hough <davidhough86@gmail.com>
2015-05-20 10:18:45 +0100

    Handling requests.

--- src/main/scala/com/boldradius/sdf/akka/RequestConsumer.scala
+++ src/main/scala/com/boldradius/sdf/akka/RequestConsumer.scala
@@ -9,10 +9,19 @@ object RequestConsumer {
 
 class RequestConsumer extends Actor with ActorLogging {
 
-  private var sessionActors = Map.empty[Long, ActorRef].withDefaultValue(context.actorOf(???))
+  private var sessionHandlers = Map.empty[Long, ActorRef]
 
   def receive: Receive = {
-    case request @ Request(id, timestamp, url, referrer, browser) =>
-      sessionActors(id) ! request
+    case request: Request => handleRequest(request)
+  }
+
+  private def handleRequest(request: Request) = {
+    if (sessionHandlers.contains(request.sessionId)) {
+      sessionHandlers(request.sessionId) ! request
+    } else {
+      val sessionHandler = context.actorOf(SessionHandlingActor.props)
+      sessionHandlers += (request.sessionId -> sessionHandler)
+      sessionHandler ! request
+    }
   }
 }
