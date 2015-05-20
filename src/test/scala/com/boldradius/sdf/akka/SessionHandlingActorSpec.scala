f10275701a59a5ed5565ff82282b91c67145e3ed
Duc Hoang <duch@cakesolutions.net>
2015-05-20 10:59:05 +0100

    Add test spec

---
+++ src/test/scala/com/boldradius/sdf/akka/SessionHandlingActorSpec.scala
@@ -0,0 +1,10 @@
+package com.boldradius.sdf.akka
+
+import akka.actor.ActorDSL._
+import akka.testkit.EventFilter
+
+class SessionHandlingActorSpec extends BaseAkkaSpec {
+  "Inactive session after 20 seconds" should {
+    "should stop that SessionHandlingActor" in {
+    }
+}
